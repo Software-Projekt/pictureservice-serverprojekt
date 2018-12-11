34acfb63de9a18b130ea13ad7ea54e1d28701bce
kai <kai@debian-linux>
2018-12-11 16:16:37 +0100

    kl

--- pictureservice-serverprojekt/src/test/Test.java
+++ pictureservice-serverprojekt/src/test/Test.java
@@ -6,7 +6,8 @@ import mb.EventMB;
 
 public class Test {
 
-	public static void main(String[] args) {
+//	public static void main(String[] args) {
+	public void main() {
 		// TODO Auto-generated method stub
 		EventMB eventMB = new EventMB();
 		eventMB.addEvent("d", "d", true, new Date(), new Date());
